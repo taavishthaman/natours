379d7d8930369dd21541155315cef93580e52c21
TaavishThaman <taavishthaman97@gmail.com>
2024-02-01 00:50:15 +0530

    cool and advanced error handling

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting Down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
@@ -19,6 +26,15 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting Down...');
+  console.log(err.name, err.message);
+  //Closing application gracefully
+  server.close(() => {
+    process.exit(1);
+  });
 });
